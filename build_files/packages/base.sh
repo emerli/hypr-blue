30a329cf490b3b7870e525716004a4f62d566f44
Emiliano Merli <emiliano.merli@gmail.com>
2026-03-31 11:15:25 +0200

    fix fonts

--- build_files/packages/base.sh
+++ build_files/packages/base.sh
@@ -21,3 +21,17 @@ dnf5 install -y \
     flatpak \
     plocate \
     firewalld
+
+
+#!/bin/bash
+set -ouex pipefail
+
+NERD_FONT_VERSION="3.3.0"
+
+curl -fLo /tmp/JetBrainsMono.tar.xz \
+    https://github.com/ryanoasis/nerd-fonts/releases/download/v${NERD_FONT_VERSION}/JetBrainsMono.tar.xz
+
+mkdir -p /usr/share/fonts/JetBrainsMonoNF
+tar -xf /tmp/JetBrainsMono.tar.xz -C /usr/share/fonts/JetBrainsMonoNF
+rm /tmp/JetBrainsMono.tar.xz
+fc-cache -fv
